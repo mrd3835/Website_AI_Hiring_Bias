1b0c429a30ede25f481b9726e3f3280fe055497e
mrd3835 <mrd3835@rit.edu>
2026-01-14 15:34:47 -0500

    uploaded images, js, and css

---
+++ js/gallery.js
@@ -0,0 +1,20 @@
+var images = [
+  { src: "images/people_header.jpg", alt: "A diverse group of cartoon people smiling" },
+  { src: "images/scale_header.jpg", alt: "A man and a woman sit on a scale, showing inequality" }
+];
+
+var i = 0;
+var slide = document.getElementById("slide");
+
+// Show the first image immediately
+slide.src = images[i].src;
+slide.alt = images[i].alt;
+
+function nextImage() {
+  i = (i + 1) % images.length; // wrap around automatically
+  slide.src = images[i].src;   // set the image source
+  slide.alt = images[i].alt;   // set the alt text
+}
+
+// Change image every 3 seconds
+setInterval(nextImage, 3000);
